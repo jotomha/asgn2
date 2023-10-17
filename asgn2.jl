e7018816294be32fd7b06c677041613267c19cde
Calvin Brown <calvinlongbrown@gmail.com>
2023-10-17 10:51:39 -0700

    modified regression fun to answer 3-7

--- asgn2.jl
+++ asgn2.jl
@@ -34,11 +34,12 @@
 
 
 # Part 2      to calvin: if it says that package GLM is not installed, just activate the project by going into package mode and then activate .
-using Random, GLM, DataFrames, CSV
+using Random, GLM, DataFrames, CSV, Statistics
 
 function regress_orthogonalize(Y, x)
     N, p = size(x)
     z = copy(x)
+    γ_vec = zeros(p)
     
     for j in 1:p
         for k in 1:(j-1)
@@ -46,15 +47,21 @@ function regress_orthogonalize(Y, x)
             z[:,j] .-= γ .* z[:,k]
         end
     end
-    
+
+    for j in 1:p
+        γ_vec[j] = (z[:,j]' * Y) / (z[:,j]' * z[:,j]) 
+    end
+
     γ_last = (z[:,p]' * Y) / (z[:,p]' * z[:,p])
-    return γ_last[1] # Extract the scalar from the 1x1 array
+	return (z, γ_vec)
+    #return γ_last[1]  Extract the scalar from the 1x1 array
 end
 
 # Question 1
 mtcars = DataFrame(CSV.File("mtcars.csv"))  # Convert csv data into dataframe
 ols_est = lm(@formula(mpg ~ wt + hp), mtcars)  # mpg as output, wt + hp as input. Not sure if you've used R before, seems to be the same syntax, but I have never used it so new to me
 coefficients = coeftable(ols_est)  # Included in GLM package
+@show coefficients
 println("hp coefficient: ", coefficients.cols[1][3])
 
 # Interpretation: For every increase in 1 horsepower, there is a 0.03 decrease in the mpg efficiency.  !! I HAVE ABSOLUTELY NO IDEA WHAT IT MEANS. THIS IS A WILD GUESS FROM WHAT I KNOW ABOUT LINEAR REGRESSION.
@@ -63,18 +70,27 @@ println("hp coefficient: ", coefficients.cols[1][3])
 # Question 2
 y = vec(mtcars.mpg) # Directly accessing a column returns in array format, i just opted to convert to vector, which probably isn't necessary
 x = [ones(32) vec(mtcars.wt) vec(mtcars.hp)]
-regression = regress_orthogonalize(y, x)  
-print("Regression: ", regression)  # returns almost the same number! is this correct?? Unclear
+z_vecs, coeff_vecs = regress_orthogonalize(y, x)
+#@show z_vecs
+@show coeff_vecs
+print("Regression: ", coeff_vecs[3])  # returns almost the same number! is this correct?? Unclear
 
 
 # Question 3: I do not know how to retrieve any of these values. Are we meant to print them within the orthogonal_regression function? 
 # Or return the whole matrix z? 
+@show coeff_vecs[1]
+# Interpretation: the coefficient for the one's vector in the least squares Regression
 
 # Question 4
+@show z_vecs[:, 2]
+@show mean(z_vecs[:, 2])
+#Interpretation: the normalized weights vector
 
 # Question 5
+@show z_vecs[:, 1]' * z_vecs[:, 3]
 
 # Question 6
+@show z_vecs[:, 2]' *z_vecs[:, 3]
 
 # Question 7
-
+@show β_k = (z_vecs[:, 3]' * y) / (z_vecs[:, 3]' * z_vecs[:, 3])
